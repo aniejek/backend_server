5866b0e1d5cd7563ad5d33f2dbe742eb803b7e7d
aniejek <nowywisniowy@gmail.com>
2018-09-08 18:22:39 +0200

    next

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -15,15 +15,16 @@ namespace Backend
         private const char KEY_AND_VALUE_SEPARATOR = '=';
 
         private const char PARAMETERS_SEPARATOR = ';';
-        private const string PARAMETER_USER = "user";
-        private const string PARAMETER_PASSWORD = "password";
-        private const string PARAMETER_QUANITY = "quanity";
-        private const string PARAMETER_INSTALLMENT = "installment";
-        private const string PARAMETER_INTEREST = "interest";
+        private const int PARAMETER_LOGIN = 0;
+        private const int PARAMETER_PASSWORD = 1;
+        private const int PARAMETER_QUANITY = 0;
+        private const int PARAMETER_INTEREST = 1;
+        private const int PARAMETER_INSTALLMENT = 2;
+        private const int PARAMETER_ACCOUNT_NUMBER = 3;
 
         private const char OPERATION_SEPARATOR = '-';
-        private const string OPERATION_LOGIN = "login";
-        private const string OPERATION_LOAN = "loan";
+        private const string OPERATION_LOGIN = "logowanie";
+        private const string OPERATION_LOAN = "pozyczka";
 
         private const int MAX_LOAN = 200000;
         private const float EXPECTED_MONTHLY_INTEREST = 0.01f;
@@ -50,52 +51,30 @@ namespace Backend
             var message = Encoding.UTF8.GetString(body);
             var operation = GetOperation(message);
             var parameters = GetParameters(message);
+            Console.WriteLine(String.Format("Otrzymano wiadomosc: {0}", message));
 
-            string user = null;
-            string password = null;
-            float quanity = 0;
-            float installment = 0;
-            float interest = 0;
-
-            foreach (string parameter in parameters)
-            {
-                var keyAndValue = GetKeyAndValue(parameter);
-                var key = keyAndValue.Item1;
-                var value = keyAndValue.Item2;
-                switch (key)
-                {
-                    case PARAMETER_USER:
-                        user = value;
-                        break;
-                    case PARAMETER_PASSWORD:
-                        password = value;
-                        break;
-                    case PARAMETER_QUANITY:
-                        quanity = float.Parse(value);
-                        break;
-                    case PARAMETER_INSTALLMENT:
-                        installment = float.Parse(value);
-                        break;
-                    case PARAMETER_INTEREST:
-                        interest = float.Parse(value);
-                        break;
-                    default:
-                        break;
-                }
-            }
             string returnString;
             switch (operation)
             {
                 case OPERATION_LOAN:
-                    returnString = LoanService(user, quanity, installment, interest);
+                    float quanity = float.Parse(parameters[PARAMETER_QUANITY]);
+                    float interest = float.Parse(parameters[PARAMETER_INTEREST]);
+                    float installment = float.Parse(parameters[PARAMETER_INSTALLMENT]);
+                    string accountNumber = parameters[PARAMETER_ACCOUNT_NUMBER];
+                    returnString = LoanService(quanity, interest, installment, accountNumber);
                     break;
                 case OPERATION_LOGIN:
-                    returnString = LoginService(user, password);
+                    string login = parameters[PARAMETER_LOGIN];
+                    string password = parameters[PARAMETER_PASSWORD];
+                    returnString = LoginService(login, password);
                     break;
                 default:
                     returnString = "404";
                     break;
             }
+
+            Console.WriteLine(String.Format("Zwracam wiadomosc: {0}", returnString));
+
             var responseBytes = Encoding.UTF8.GetBytes(returnString);
             var replyProps = ((Model)model).CreateBasicProperties();
             replyProps.CorrelationId = ea.BasicProperties.CorrelationId;
@@ -135,20 +114,20 @@ namespace Backend
             return String.Format("201-{0};{1};{2};{3}", number, account, name, surname);
         }
 
-        private static string LoanService(string user, float quanity, float installment, float interest)
+        private static string LoanService(float quanity, float interest, float installment, string accountNumber)
         {
-            if (quanity != 0 && installment != 0 && interest != 0 && !(user is null))
+            if (quanity != 0 && installment != 0 && interest != 0 && !(accountNumber is null))
             {
                 float expected_interest = EXPECTED_MONTHLY_INTEREST * (1 + quanity / MAX_LOAN);
                 if (interest / installment >= expected_interest)
                 {
-                    return TakeLoan(user, quanity, installment, interest);
+                    return TakeLoan(accountNumber, quanity, installment, interest);
                 }
             }
             return "402";
         }
 
-        private static string TakeLoan(string user, float quanity, float installment, float interest)
+        private static string TakeLoan(string accountNumber, float quanity, float installment, float interest)
         {
             var sqlConnection = new SqlConnection
             {
@@ -163,20 +142,21 @@ namespace Backend
             };
             sqlConnection.Open();
             var cmd = sqlConnection.CreateCommand();
+            cmd.CommandText = String.Format("select login from [dbo.Users] where number = {0}", accountNumber);
+            string user = (string)cmd.ExecuteScalar();
             cmd.CommandText = String.Format("insert into [dbo.loans] (user, quanity, interest, installment);" +
                 " values({0}, {1}, {2}, {3})", user, quanity, interest, installment);
             cmd.ExecuteNonQuery();
-            cmd.CommandText = String.Format("select number, account, name, surname from [dbo.Users] where login = {0};", user);
+            cmd.CommandText = String.Format("select account, name, surname from [dbo.Users] where login = {0};", user);
             var executeReader = cmd.ExecuteReader();
-            string number = executeReader.GetString(0);
-            float account = executeReader.GetFloat(1);
-            string name = executeReader.GetString(2);
-            string surname = executeReader.GetString(3);
+            float account = executeReader.GetFloat(0);
+            string name = executeReader.GetString(1);
+            string surname = executeReader.GetString(2);
             account += quanity;
             cmd.CommandText = String.Format("update [dbo.Users] set account={0} where login={1};", account, user);
             cmd.ExecuteNonQuery();
             sqlConnection.Close();
-            return String.Format("201-{0};{1};{2};{3}", number, account, name, surname);
+            return String.Format("201-{0};{1};{2};{3}", accountNumber, account, name, surname);
         }
 
         static void Main(string[] args)
@@ -186,7 +166,7 @@ namespace Backend
                 UserName = "guest",
                 Password = "guest",
                 HostName = "localhost",
-                VirtualHost = "103057"
+                VirtualHost = "/"
             };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
